f81a73f7c4e070e09d9378e6aa5810431683330f
rocket-builder <53790403+rocket-builder@users.noreply.github.com>
2022-02-08 16:00:08 +0300

    Initial commit

---
+++ src/main/java/com/anthill/OFStatisticsAPI/OfStatisticsApiApplication.java
@@ -0,0 +1,13 @@
+package com.anthill.OFStatisticsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfStatisticsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfStatisticsApiApplication.class, args);
+	}
+
+}
