dc53dddebc522cc5dce27d5694a1e51006e74a5a
rocket-builder <53790403+rocket-builder@users.noreply.github.com>
2022-02-08 17:17:39 +0300

    Update MainController.java

--- src/main/java/com/anthill/OFStatisticsAPI/controllers/MainController.java
+++ src/main/java/com/anthill/OFStatisticsAPI/controllers/MainController.java
@@ -1,8 +1,10 @@
 package com.anthill.OFStatisticsAPI.controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Index")
 @RestController
 public class MainController {
 
