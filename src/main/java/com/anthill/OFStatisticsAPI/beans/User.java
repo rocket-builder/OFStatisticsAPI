8acc98678f82708a0803b9986197d7cc3b95970a
rocket-builder <53790403+rocket-builder@users.noreply.github.com>
2022-02-08 17:38:05 +0300

    add base implementation

--- src/main/java/com/anthill/OFStatisticsAPI/beans/User.java
+++ src/main/java/com/anthill/OFStatisticsAPI/beans/User.java
@@ -1,11 +1,14 @@
 package com.anthill.OFStatisticsAPI.beans;
 
 
+import com.anthill.OFStatisticsAPI.enums.Role;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
 @NoArgsConstructor
 @Getter @Setter
@@ -13,4 +16,7 @@ import javax.persistence.Entity;
 public class User extends AbstractEntity {
 
     private String login, password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.WORKER;
 }
--- src/main/java/com/anthill/OFStatisticsAPI/controllers/MainController.java
+++ src/main/java/com/anthill/OFStatisticsAPI/controllers/MainController.java
@@ -3,7 +3,6 @@ package com.anthill.OFStatisticsAPI.controllers;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Index")
 @Controller
