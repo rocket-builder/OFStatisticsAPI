92112df14815418659ad173a2750e366e8417df4
rocket-builder <53790403+rocket-builder@users.noreply.github.com>
2022-02-08 17:23:01 +0300

    Update MainController.java

--- src/main/java/com/anthill/OFStatisticsAPI/controllers/MainController.java
+++ src/main/java/com/anthill/OFStatisticsAPI/controllers/MainController.java
@@ -1,16 +1,17 @@
 package com.anthill.OFStatisticsAPI.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Index")
-@RestController
+@Controller
 public class MainController {
 
     @GetMapping("/")
     public String index(){
 
-        return "OFStatistics API works normally!";
+        return "swagger-ui.html";
     }
 }
